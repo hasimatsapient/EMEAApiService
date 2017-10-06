86fc4106d8c0b00e95cb21cb2d8b52158123bdb4
hasimatsapient <hasim.mollah@gmail.com>
2017-10-06 12:19:18 +0530

    Change in service class

--- EMEAApiService/src/main/java/com/emea/service/AccountInfoService.java
+++ EMEAApiService/src/main/java/com/emea/service/AccountInfoService.java
@@ -1,13 +1,8 @@
 package com.emea.service;
 
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.emea.dao.AccountInfoDao;
 import com.emea.dto.AccountInfoVo;
-import com.emea.model.AccountInfoBo;
-import com.emea.util.CommonUtil;
 
 /**
  * Service to fetch account information
@@ -15,26 +10,14 @@ import com.emea.util.CommonUtil;
  *
  */
 @Service
-public class AccountInfoService {
-    private static final Logger LOG = Logger
-            .getLogger(AccountInfoService.class);
-    @Autowired
-    AccountInfoDao accountInfoDao;
-
+public interface AccountInfoService {
     /**
      * Method to get account information.
      * @param accountNumber
      * @param sortCode
      * @return
      */
-    public AccountInfoVo getAccountDetails(long accountNumber, long sortCode){
-        LOG.info("Started executing getAccountDetails");
-        AccountInfoBo accountInfoBo =accountInfoDao.getAccountInfoByAccountNumberAndSortCode(accountNumber, sortCode);
-        
-        AccountInfoVo accountInfoVo = CommonUtil. convertAccountInfoBoToVo(accountInfoBo);
-        LOG.info("Finished executing getAccountDetails");
-        return accountInfoVo;
-    }
+    public AccountInfoVo getAccountDetails(long accountNumber, long sortCode);
     
    
 }
---
+++ EMEAApiService/src/main/java/com/emea/service/AccountInfoServiceImpl.java
@@ -0,0 +1,40 @@
+package com.emea.service;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.emea.dao.AccountInfoDao;
+import com.emea.dto.AccountInfoVo;
+import com.emea.model.AccountInfoBo;
+import com.emea.util.CommonUtil;
+
+/**
+ * Service to fetch account information
+ * @author hmolla
+ *
+ */
+@Service
+public class AccountInfoServiceImpl implements AccountInfoService{
+    private static final Logger LOG = Logger
+            .getLogger(AccountInfoServiceImpl.class);
+    @Autowired
+    AccountInfoDao accountInfoDao;
+
+    /**
+     * Method to get account information.
+     * @param accountNumber
+     * @param sortCode
+     * @return
+     */
+    public AccountInfoVo getAccountDetails(long accountNumber, long sortCode){
+        LOG.info("Started executing getAccountDetails");
+        AccountInfoBo accountInfoBo =accountInfoDao.getAccountInfoByAccountNumberAndSortCode(accountNumber, sortCode);
+        
+        AccountInfoVo accountInfoVo = CommonUtil. convertAccountInfoBoToVo(accountInfoBo);
+        LOG.info("Finished executing getAccountDetails");
+        return accountInfoVo;
+    }
+    
+   
+}
