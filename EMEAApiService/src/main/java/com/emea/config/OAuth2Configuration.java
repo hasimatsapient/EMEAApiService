766496a0cc2376521cf1c63c2d4b88c132104ab0
hasimatsapient <hasim.mollah@gmail.com>
2017-10-10 11:55:03 +0530

    change in user authorization logic

--- EMEAApiService/src/main/java/com/emea/config/OAuth2Configuration.java
+++ EMEAApiService/src/main/java/com/emea/config/OAuth2Configuration.java
@@ -1,11 +1,10 @@
 package com.emea.config;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.sql.DataSource;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
@@ -15,8 +14,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -31,18 +30,21 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import com.emea.security.Authorities;
+import com.emea.controller.CustomControllerAdvice;
 import com.emea.security.CustomAuthenticationEntryPoint;
 import com.emea.security.CustomLogoutSuccessHandler;
 
 @Configuration
 public class OAuth2Configuration {
-
+    private static final Logger LOG = Logger
+            .getLogger(OAuth2Configuration.class);
     @Configuration
     @EnableResourceServer
     protected static class ResourceServerConfiguration
             extends
                 ResourceServerConfigurerAdapter {
+        
+       
 
         @Autowired
         private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
@@ -83,8 +85,16 @@ public class OAuth2Configuration {
         private static final String PROP_CLIENTID = "clientid";
         private static final String PROP_SECRET = "secret";
         private static final String PROP_TOKEN_VALIDITY_SECONDS = "tokenValidityInSeconds";
+        protected static final String AUTHORIZATION_CODE = "authorization_code";
+        protected static final String PASSWORD = "password";
+        protected static final String REFRESH_TOKEN = "refresh_token";
+        protected static final String CLIENT_CREDENTIAL = "client_credentials";
+        protected static final String READ = "read";
+        protected static final String WRITE = "write";
 
         private RelaxedPropertyResolver propertyResolver;
+        @Autowired
+        private UserDetailsService userDetailsService;
 
         @Autowired
         private DataSource dataSource;
@@ -112,22 +122,28 @@ public class OAuth2Configuration {
                 @Override
                 public ClientDetails loadClientByClientId(String clientId)
                         throws ClientRegistrationException {
-                    BaseClientDetails details = new BaseClientDetails();
-                    details.setClientId(clientId);
-                    details.setAuthorizedGrantTypes(Arrays.asList(
-                            "authorization_code", "password", "refresh_token",
-                            "client_credentials"));
-                    details.setScope(Arrays.asList("read, trust", "write"));
-                    details.setClientSecret(propertyResolver
-                            .getProperty(PROP_SECRET));
-                    Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
-                    authorities.add(new SimpleGrantedAuthority(
-                            Authorities.ROLE_CLIENT.toString()));
-                    authorities.add(new SimpleGrantedAuthority(
-                            Authorities.ROLE_ADMIN.toString()));
-                    authorities.add(new SimpleGrantedAuthority(
-                            Authorities.ROLE_USER.toString()));
-                    details.setAuthorities(authorities);
+                   UserDetails user= userDetailsService.loadUserByUsername(clientId);
+                   BaseClientDetails details = new BaseClientDetails();
+                   details.setClientId(clientId);
+                   details.setAuthorizedGrantTypes(Arrays.asList(
+                           AUTHORIZATION_CODE, PASSWORD, REFRESH_TOKEN,
+                           CLIENT_CREDENTIAL));
+                   details.setScope(Arrays.asList(READ,  WRITE));
+                   details.setClientSecret(propertyResolver
+                           .getProperty(PROP_SECRET));
+                   details.setAuthorities(user.getAuthorities());
+                   
+                   Integer tokenValidityInSeconds=0;
+                   
+                   try {
+                       tokenValidityInSeconds= Integer.valueOf( propertyResolver
+                               .getProperty(PROP_TOKEN_VALIDITY_SECONDS));
+                   } catch (NumberFormatException e) {
+                       LOG.error("Invalid token validity time configured setting to default 0", e);
+                   }
+                   
+                   details.setRefreshTokenValiditySeconds(tokenValidityInSeconds);
+                    
                     return details;
                 }
             };
